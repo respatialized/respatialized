ad43d92b55377d5c48a279e919e77f8c38b0c4fc
Andrew Foltz-Morrison <afoltzm@gmail.com>
2019-03-17 09:24:38 -0400

    a placeholder page so the make command works

--- books.html.pm
+++ books.html.pm
@@ -1,15 +1,16 @@
 #lang Pollen
 
 ◊(define-meta title "Books to Read")
-◊(define-meta updated "2016-09-30")
+◊(define-meta updated "2019-03-17")
 
 ◊strong{"Best-of" Lists:}
 
 ◊ul{
-    ◊item{◊link["/posts/50-books-every-child-should-read.html"]{50 Books Every Child Should Read} — picks from three of Britain’s leading children’s authors and two in-house book experts at the ◊emph{Independent}.}
-    ◊item{◊link["/posts/33-best-science-fiction-books.html"]{The 33 Best Science Fiction Novels Ever} collected from an Ask Reddit post.}
+    ◊item{The Warmth Of Other Suns}
 }
 
 ◊strong{Good Recent Reads}
 
-◊amazon["0575127724"]{◊image["book-thething.jpg"]{The Thing Itself}} ◊amazon["1455561789"]{◊image["book-beforethefall.jpg"]{Before the Fall}} ◊amazon["0547572484"]{◊image["book-maninhighcastle.jpg"]{Man in High Castle}}
+◊ul{
+  ◊item{Fossil Capital}
+}
