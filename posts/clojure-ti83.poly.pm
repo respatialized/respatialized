f6b1c23e308758866f51e64166ae92438c3a04a6
Andrew Foltz-Morrison <afoltzm@gmail.com>
2019-05-23 09:59:09 -0400

    1 column only looks good on a hi-dpi screen

--- posts/clojure-ti83.poly.pm
+++ posts/clojure-ti83.poly.pm
@@ -13,5 +13,5 @@ Clojure is an unusually expressive programming language. Its minimal syntax and 
 (#{3 4 5} 2)
 (map #{3 4 5} [1 2 3 4 5])
 (filter #{3 4 5} [1 2 3 4 5])}}}
-◊c[#:span "1" #:span-s "row" #:class "small"]{
+◊c[#:span "2" #:span-s "row" #:class "small"]{
 Just like that. As soon as the idea came to me I found that I could call a data structure as a function and filter the data I wanted with no syntactic overhead. This expressive power means that it basically takes me as long to ◊em{implement} a function as it does to look up how it might be done on a graphic calculator. Plus, my laptop has a color screen.}
