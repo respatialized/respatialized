b36d5521247bad8c523e87ddf8fab65d239e1d7e
Andrew Foltz-Morrison <andrew.foltz-morrison@sitomobile.com>
2019-05-24 12:43:16 -0400

    a quick speculation

---
+++ posts/information-cocoon.poly.pm
@@ -0,0 +1,17 @@
+#lang pollen
+
+◊(define-meta title "Reifying the Filter Bubble")
+◊(define-meta published "2019-05-24")
+◊(define-meta topics "infrastructure,speculation,climate")
+◊c[#:span "1-3" #:span-s "row"]{◊section{Making the metaphor concrete}
+Just as the frequency and severity of major storms increases, the United States government is considering a change in technology that would blanket us in an information shroud: opening up the 24 gHz frequency band to 5G devices. The filter bubbles we've built in digital space will soon be so amplified that they will begin serving the same role in the physical space.
+
+◊link["https://grist.org/article/5g-networks-could-throw-weather-forecasting-into-chaos/"]{Grist reports:} "On Capitol Hill Thursday, NOAA’s acting chief, Neil Jacobs, said that interference from 5G wireless phones could reduce the accuracy of forecasts by 30 percent."
+
+Signals that detect water vapor operate at a frequency of 23.8 gHz, vanishingly close to the band that would quickly be overwhelmed by 4K streams from influencers, memes so emphemeral they won't be understood next week, and millisecond-resolution updates from smart refrigerators. The digital signals carried by 5G can be assigned to an arbitrary frequency band - the analog signals of water vapor cannot. Streams of unnecessary and irrelevant information about virtual worlds would crowd out vital information about the physical world in a time of climate crisis. The latter has much more aggressive ways of announcing itself, however.
+} ◊c[#:span "4-6" #:span-s "row"]{◊section{A Likely Scenario}
+
+5G is adopted without compromise. It takes a few years for its bandwidth to reach its full capacity, but long before that, a hurricane forms, threatening an indeterminate area. It draws closer to land in ways that are now harder to predict. As the skies on land darken, our ability to reason about their potential movements follows. A cacophony of nervous texts between relatives and phone calls between emergency agencies increases dims the signal that can be read from the sky. An unexpected landfall increases the panic and the last-minute coordination between emergency repsonse agencies, leading to a feedback loop of further signal interference.
+
+Eventually, the storm begins taking down cell towers, and other buildings with them. From a distance, the meteorologists at last have an unimpeded signal - but only from the areas where it's too late.
+}
