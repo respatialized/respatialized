7655c5629175e6b3e44c9240a4bcd727b4699035
Andrew Foltz-Morrison <andrew-eng@gastrograph.com>
2019-07-05 10:01:43 -0400

    quick post on a train

---
+++ posts/working-definition.poly.pm
@@ -0,0 +1,12 @@
+#lang pollen
+◊(define-meta title "A Working Definition")
+◊(define-meta published "2019-07-05")
+◊(define-meta topics "ecology,politics")
+◊c[#:span "1-4" #:span-s "row"]{
+I had a conversation recently about ecological limits that ended up here:
+
+"Obviously carrying capacity is a political question, because it turns on assumptions about what kind of life people are entitled to, and what resources can be used in what way to support that life. And it will take a way of modeling that ordinary people can participate in so that technocrats aren't just deciding by fiat what carrying capacity means, but also so that everyone has shared assumptions about the actual limits we need to live within."
+
+"Yep, that sounds like what we need to do. Sounds like a huge task."
+
+"Yeah. You might even call it 'socialism.'"}
