b75a2425e1cd4e62c92a0011ec066c69a6c7afd6
Andrew Foltz-Morrison <andrew-eng@gastrograph.com>
2019-07-06 18:12:32 -0400

    adding cljs html scraping methods to post

--- posts/clojure-ti83-tidy.poly.pm
+++ posts/clojure-ti83-tidy.poly.pm
@@ -7,7 +7,7 @@
 In order to do my ◊link["https://cnx.org/contents/MBiUQmmY@23.31:i_O99VEg/1-6-Sampling-Experiment"]{self-assigned math homework}, I needed to ensure the data provided are in an appropriate format. Here's how the book provides the data.
 }
 ◊c[#:span "row" #:span-s "row"]{
-◊table[#:class "small"]{
+◊div[#:id "table-1-20-data"]{◊table{
 Entree Cost | Under $10 | $10 to under $15 | $15 to under $20 | Over $20
 San Jose | El Abuelo Taq, Pasta Mia, Emma’s Express, Bamboo Hut | Emperor’s Guard, Creekside Inn | Agenda, Gervais, Miro’s | Blake’s, Eulipia, Hayes Mansion, Germania
 Palo Alto | Senor Taco, Olive Garden, Taxi’s | Ming’s, P.A. Joe’s, Stickney’s | Scott’s Seafood, Poolside Grill, Fish Market | Sundance Mine, Maddalena’s, Spago’s
@@ -16,7 +16,7 @@ Mountain View | Maharaja, New Ma’s, Thai-Rific, Garden Fresh | Amber Indian, L
 Cupertino | Hobees, Hung Fu, Samrat, Panda Express | Santa Barb. Grill, Mand. Gourmet, Bombay Oven, Kathmandu West | Fontana’s, Blue Pheasant | Hamasushi, Helios
 Sunnyvale | Chekijababi, Taj India, Full Throttle, Tia Juana, Lemon Grass | Pacific Fresh, Charley Brown’s, Cafe Cameroon, Faz, Aruba’s | Lion & Compass, The Palace, Beau Sejour
 Santa Clara | Rangoli, Armadillo Willy’s, Thai Pepper, Pasand | Arthur’s, Katie’s Cafe, Pedro’s, La Galleria | Birk’s, Truya Sushi, Valley Plaza | Lakeside, Mariani’s
-}
+}}
 }
 ◊c[#:span "1-4" #:span-s "row"]{
 Perhaps easy to pick items from if you're counting them out by hand, but not the most helpful format for a statistical program to manipulate. Multiple values are bunched together in single table cells and identifying information is split across both a header row and the first column. Not much could be done with this data in its current form. In short, it needs to be tidied up.
@@ -30,17 +30,17 @@ Hadley Wickham provides a helpful formal definition of tidy data in his ◊link[
 }
 }
 ◊c[#:span "1-4"]{
-I can use clojure.spec to define a schema for tidy datasets, and enforce it on incoming data and the data we pass to functions. Defining one requires a choice about how to represent tidy data.
+I can use clojure.spec to define a schema for tidy datasets, and enforce it on incoming data and the data passed to functions. Defining one requires a choice about how to represent tidy data.
 }
 ◊c[#:span "1-2"]{it can either be a map of column names and column vectors:
-◊code{
+◊codeblock{
 {:cost [15 20 10]
  :city ["Palo Alto" "Mountain View" "Sunnyvale"]
  :name ["Poolside Grill" "Le Petit Bistro" "Pedro's"]
  }
 }}
 ◊c[#:span "3-4"]{or it can be a vector of column name-column value maps:
-◊code{
+◊codeblock{
 [
  {:cost 15 :city "Palo Alto" :name "Poolside Grill"}
  {:cost 20 :city "Mountain View" :name "Le Petit Bistro"}
@@ -78,5 +78,16 @@ I chose the former for two reasons: it's less verbose, and it makes it possible 
  }))
 }}
 
-Now that I have a sample of the data that conforms to the spec, I can scrape the data out of the table and reshape it to conform to the spec.
-}
+Now that I have a sample of the data that conforms to the ::tidy? spec, I can scrape the data out of the table and reshape it into a tidy form. ◊link["http://www.3jane.co.uk/posts/breaking-out-of-klipse"]{3Jane has some helpful info} on using Klipse code blocks to access HTML DOM elements in ClojureScript, which I can adapt to pull the data out of this table by its div ID.
+
+◊pre{◊code[#:class "language-klipse"]{
+(defn scrape-element-id [id]
+  (->
+    js/document
+    ; get first panel on the page
+    (.getElementById id)
+    (.-innerHTML)
+    ))
+
+(def table-data (scrape-element-id "table-1-20-data"))
+}}}
