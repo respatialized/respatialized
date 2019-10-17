a3f42c192e826d1cdedf8ab416b8ae38845755ff
Andrew Foltz-Morrison <afoltzm@gmail.com>
2019-10-17 09:50:10 -0400

    initial outline

---
+++ build/drafts/ai-and-labor.poly.pm
@@ -0,0 +1,11 @@
+Notes on AI and labor
+
+- Moore's law gets soaked by exponential increases in the amount of CPU time to achieve linear improvements in ability of a model to perform its tasks.
+- [OpenAI article admitting this]
+- To the extent that AI gets "smarter", it's largely due to the fact that we're getting better at throwing more resources at the problem.
+- [fact about carbon consumption of deep learning models]
+- Back in the day we thought progress in AI would consist of individual computers doing better with less resources - that doesn't describe the "state of the art" at all.
+- AI researchers like games because they provide a bounded problem space with immediate feedback. The real world is not as generous.
+- So how do we get that feedback in the real world, at the scale required for massive distributed models to avoid overfitting? Data factories.
+- [Vi Hart quotation about the human labor a SV company was pretending to perform with AI]
+- Hart's essay doesn't say it in these terms, but she's talking about a theme familiar to Marxian economists - the reduction of skilled to simple labor by capital.
