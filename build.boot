592468d5eac795dedd668a546b887fe491b1ab0b
Andrew Foltz-Morrison <afoltzm@gmail.com>
2019-11-13 09:48:11 -0500

    local version of tachyons and custom fonts

--- build.boot
+++ build.boot
@@ -1,6 +1,6 @@
 (set-env!
  :source-paths #{"src"}
- :resource-paths #{"content"}
+ :resource-paths #{"content" "resources"}
  :dependencies '[[perun "0.4.3-SNAPSHOT" :scope "test"]
                  [hiccup "2.0.0-alpha2" :exclusions [org.clojure/clojure]]
                  [pandeiro/boot-http "0.8.3" :exclusions [org.clojure/clojure]]])
--- src/respatialized/core.clj
+++ src/respatialized/core.clj
@@ -11,7 +11,8 @@
    [:meta {:charset "utf-8"}]
    [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
    [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, user-scalable=no"}]
-   (hp/include-css "https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css")]
+   (hp/include-css "/css/fonts.css")
+   (hp/include-css "/css/tachyons.min.css")]
   )
 
 
