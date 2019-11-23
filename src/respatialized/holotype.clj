55f6e76422dc9e3f489aa4f6801905c283c048de
Andrew Foltz-Morrison <afoltzm@gmail.com>
2019-11-23 14:20:41 -0500

    replace the boring example with a cooler one

--- src/respatialized/holotype.clj
+++ src/respatialized/holotype.clj
@@ -15,7 +15,8 @@
 (defn svg-pt [[^Double x ^Double y]]
   [:circle
    {:cx (m/* 500 (m/+ 0.5 (m// x 4.0)))
-    :cy (m/* 700 (m/+ 0.5 (m// y 4.0)))
+    :cy (m/* 700 (m/+ 0.5 (m// y 5.0)))
+    :style {:opacity 0.2}
    :r 1}])
 
 (def pts (into [:svg {:id "clifford" :width 500 :height 700}]
@@ -23,6 +24,13 @@
                     (take 20000
                           (iterate
                            (clifford 1.7 1.7 0.6 1.2)
+                           [0.7 0.7])))))
+
+(def pts2 (into [:svg {:id "clifford" :width 500 :height 700}]
+               (map svg-pt
+                    (take 20000
+                          (iterate
+                           (clifford 1.7 2.7 0.9 1.2)
                            [0.7 0.7])))))
 
 (defn one
@@ -33,15 +41,11 @@
     (header "HOLOTYPE1")
     [:body {:class "ml3 basier-mono bg-mid-gray"}
      [:div {:class "f1 b white"} "HOLOTYPE//1"]
-     [:div [:p "an example of using Clojure alone to render a Perun post."]]
-     [:div [:p "using Hiccup data structures, SVG can be generated using computational methods at compile time."]]
-     [:svg {:id "holocanvas"
-               :width 500
-               :height 700
-               :class "ba"}
-      [:circle {:cx 50 :cy 50 :r "40"}]]
-     [:div]
-     pts]
-    [:footer {:class "white mb7"}
-     [:div [:a {:href "/"} "Home"]]]]))
+     [:div [:p "an example of using clojure alone to render a Perun post:"]]
+     [:div [:p "render svg at compile time using hiccup data structures"]]
+     pts
+     pts2
+     [:div [:p "CAUTION RECOMMENDED // RESULTING PAGE WEIGHS 3.5MB"]]]
+    [:footer {:class "white f3 mb7"}
+     [:div [:a {:href "/"} "//RESPATIALIZED"]]]]))
 
