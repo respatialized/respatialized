49816e7b494aa9c6bbdcb849a23662ec22161ccf
Andrew Foltz-Morrison <afoltzm@gmail.com>
2019-12-28 16:47:24 -0500

    trying to get a feel for representing prose as a sequence of maps

--- src/respatialized/io.clj
+++ src/respatialized/io.clj
@@ -2,6 +2,28 @@
   (:require [clojure.string :as str]
             [clojure.edn :as edn]))
 
+(defn load-edn
+  "Load edn from an io/reader source (filename or io/resource)."
+  [source]
+  (try
+    (with-open [r (clojure.java.io/reader source)]
+      (edn/read (java.io.PushbackReader. r)))
+
+    (catch java.io.IOException e
+      (printf "Couldn't open '%s': %s\n" source (.getMessage e)))
+    (catch RuntimeException e
+      (printf "Error parsing edn file '%s': %s\n" source (.getMessage e)))))
+
+
+(defn file->edn
+  "Parses the given file into EDN. Assumes no top-level delimiter"
+  [file]
+  (with-open [edn-file (clojure.java.io/reader file)]
+    (-> (clojure.java.io/reader file)
+        line-seq
+        doall
+        (#(map clojure.edn/read-string %)))))
+
 (defn txt->edn
   "Parses the given text into clojure data structures.
    By default, chunks each paragraph into its own item."
@@ -9,4 +31,4 @@
   (->> (str/split text #"\n")
        (filter #(not (empty? %)))
        (map #(str "\"" % "\""))
-       (map edn/read-string)))
+       (map (fn [line] {:text (edn/read-string line)}))))
--- test/respatialized/io_test.clj
+++ test/respatialized/io_test.clj
@@ -1,13 +1,30 @@
 (ns respatialized.io-test
   (:require
    [clojure.test :as t]
+   [clojure.edn :as edn]
    [respatialized.io :refer :all]))
 
-(def test-txt (slurp "resources/building-with-earth-excerpt.txt"))
+(def edn-text
+  '({:text
+  "In nearly all hot-arid and temperate climates, earth has always been the most prevalent building material. Even today, one third of the human population resides in earthen houses; in developing countries this figure is more than one half. It has proven impossible to fulfil the immense requirements for shelter in the developing countries with industrial building materials, i.e. brick, concrete and steel, nor with industrialised construction techniques. Worldwide, no region is endowed with the productive capacity or financial resources needed to satisfy this demand. In the developing countries, requirements for shelter can be met only by using local building materials and relying on do-it-yourself construction techniques. Earth is the most important natural building material, and it is available in most regions of the world. It is frequently obtained directly from the building site when excavating foundations or basements. In the industrialised countries, careless exploitation of resources and centralised capital combined with energy-intensive production is not only wasteful; it also pollutes the environment and increases unemployment. In these countries, earth is being revived as a building material."}
+ {:text
+  "Increasingly, people when building homes demand energy- and cost-effective buildings that emphasise a healthy, balanced indoor climate. They are coming to realise that mud, as a natural building material, is superior to industrial building materials such as concrete, brick and lime-sandstone. Newly developed, advanced earth building techniques demonstrate the value of earth not only in do-it-yourself construction, but also for industrialised construction involving contractors."}
+ {:text
+  "This handbook presents the basic theoretical data concerning this material, and it provides the necessary guidelines, based on scientific research and practical experience, for applying it in a variety of contexts."}))
+
+(def test-plaintext (slurp "resources/building-with-earth-excerpt.txt"))
+
+(def test-edn-file "resources/building-with-earth-excerpt.edn")
 
 (t/deftest text-parsers
   (t/testing "basic parsing"
-    (t/is (= 3 (count (txt->edn test-txt))))
+    (t/is (= 3 (count (txt->edn test-plaintext))))
+    (t/is (= edn-text (file->edn test-edn-file)))
+    (t/is (= edn-text (txt->edn test-plaintext) (file->edn test-edn-file)))
     )
 
-  (t/testing "identical output for regular and malformed inputs"))
+  (t/testing "identical output for regular and malformed inputs")
+
+
+  (t/testing "identical output for plaintext and hiccup-style inputs")
+  )
