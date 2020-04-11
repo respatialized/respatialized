4c964236ee24633bb91e7b4559aa547d15d80f33
Andrew Foltz-Morrison <afoltzm@gmail.com>
2020-04-11 15:34:14 -0400

    removing ash-ra-template for now

--- src/respatialized/build.clj
+++ src/respatialized/build.clj
@@ -1,7 +1,5 @@
 (ns respatialized.build
   (:require
-   [vivid.art :as art]
-   [vivid.art.parse :refer [parse]]
    [comb.template :as template]
    [clojure.java.io :as io]
    [hiccup.core :refer [html]]
@@ -11,51 +9,10 @@
    [respatialized.holotype :as holotype]
    [clojure.string :as str]))
 
-(def art-config
-  {:dependencies
-   {'hiccup {:mvn/version "2.0.0-alpha2"}
-    'org.clojure/clojure {:mvn/version "1.10.0"}
-    'respatialized {:mvn/version "SNAPSHOT"}
-    'clojure2d {:mvn/version "1.2.0-SNAPSHOT"}
-    'generateme/fastmath {:mvn/version "1.4.0-SNAPSHOT"}
-    }
-   })
-
-(defn check-art-form
-  ([form pre config] (art/render (str pre form) config))
-  ([form pre] (check-art-form form pre art-config))
-  ([form] (check-art-form form "<% (require '[hiccup.core :refer [html]] '[respatialized.render :refer :all])%>")))
-
-(defn check-art-file
-  ([path pre config] (check-art-form (slurp path) pre config))
-  ([path pre] (check-art-form (slurp path) pre art-config))
-  ([path] (check-art-form (slurp path) "")))
-
-(defn render-art-file [content]
-  (page (art/render content art-config)))
-
-(defn render-art-files
-  ([art-files out-dir]
-   (doseq [f art-files]
-     (let [out-file (-> f
-                        io/file
-                        (.getName)
-                        (.toString)
-                        (str/split art/art-filename-suffix-regex)
-                        first
-                        (#(str out-dir "/" %)))]
-       (println "Rendering" (.toString out-file))
-       (-> f
-           slurp
-           render-art-file
-           postprocess/detect-paragraphs
-           (#(spit out-file %))))))
-  ([art-files] (render-art-files art-files "public")))
-
 (def template-suffix ".ct")
 (def template-suffix-regex #"#*[.]ct$")
 
-(defn render-comb-file
+(defn render-template-file
   ([path page-fn out-dir]
    (let [out-file (-> path
                       io/file
@@ -71,9 +28,9 @@
        postprocess/detect-paragraphs
        (#(spit out-file %))
        )))
-  ([path page-fn] (render-comb-file path page-fn "public"))
+  ([path page-fn] (render-template-file path page-fn "public"))
   ([path]
-   (render-comb-file path page "public")))
+   (render-template-file path page "public")))
 
 (defn get-template-files [dir suffix]
   (->> dir
@@ -83,28 +40,25 @@
                      (.endsWith (.toString %) suffix)))
        (map #(.toString %))))
 
-(defn render-comb-files
-  ([dir page-fn out-dir]
-   (let [template-files (get-template-files dir template-suffix)]
-     (doseq [f template-files]
-       (render-comb-file f page-fn out-dir))))
-  ([dir page-fn] (render-comb-files dir page-fn "public"))
-  ([dir] (render-comb-files dir page "public"))
-  ([] (render-comb-files "content")))
+(defn render-template-files
+  "Renders the given files. Renders all in the content dir when called without args."
+  ([template-files page-fn out-dir]
+   (doseq [f template-files]
+     (render-template-file f page-fn out-dir)))
+  ([template-files page-fn] (render-template-files template-files page-fn "public"))
+  ([template-files] (render-template-files template-files page "public"))
+  ([] (render-template-files (get-template-files "content" template-suffix))))
 
-(defn get-art-files [dir]
-  (->> dir
-       io/file
-       file-seq
-       (filter #(and (.isFile %)
-                     (.endsWith (.toString %) art/art-filename-suffix)))
-       (map #(.toString %))))
-
-(defn render-all-art [in-dir out-dir]
-  (let [art-files (get-art-files in-dir)]
-    (render-art-files art-files out-dir)))
+(defn load-deps []
+  (do (require '[respatialized.render :refer :all]
+               '[respatialized.holotype :as holotype]
+               '[hiccup.page :as hp]
+               '[hiccup.core :refer [html]])))
 
 (defn -main
   ([]
-   (render-all-art "content" "public"))
-  ([& files] (render-art-files files "public")))
+   (load-deps)
+   (render-template-files))
+  ([& files]
+   (load-deps)
+   (render-template-files files)))
