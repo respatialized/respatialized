3d5d93ebe149720230774aaf1915c4eedba0997e
Andrew Foltz-Morrison <afoltzm@gmail.com>
2022-03-30 10:14:00 -0400

    Further color debugging
    It seems to have something to do with the image encoding; the dtype-next
    test image seems to produce correct pixel values.

--- test/respatialized/colors_test.clj
+++ test/respatialized/colors_test.clj
@@ -3,6 +3,12 @@
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
             [clojure.test.check.clojure-test :as tc]
+            [tech.v3.datatype :as dtype]
+            [tech.v3.tensor :as tensor]
+            [tech.v3.libs.buffered-image :as dtype-img]
+            [clojure2d.core :as clj2d]
+            [clojure2d.color :as clj2d-color]
+            [clojure2d.pixels :as clj2d-pixels]
             [clojure.test :as t]))
 
 ;; general idea: use manual coordinates to verify color
@@ -10,7 +16,79 @@
 (def test-img-url "https://live.staticflickr.com/65535/51913654989_7490d04a5a_4k.jpg")
 (def test-coords [1000 1000])
 
+(def off-white (clj2d-color/color "#f4f0e9") )
+
+(test-img-tens 50 50)
+
+(def test-img-clj2d (clj2d/load-url-image test-img-url))
+(def test-img-dtype (dtype-img/load test-img-url))
+(def test-img-tens (tensor/ensure-tensor test-img-dtype))
+
 (comment
+  (-> "https://github.com/cnuernber/dtype-next/blob/master/test/data/test.jpg?raw=true"
+      (dtype-img/load )
+      (tensor/ensure-tensor)
+      (tensor/mget 10 10))
+
+
+  (-> "https://github.com/cnuernber/dtype-next/blob/master/test/data/test.jpg?raw=true"
+      (dtype-img/load )
+      (dtype-img/image-channel-format))
+
+  (-> test-img-url
+      (dtype-img/load )
+      (dtype-img/image-channel-format))
+
+  (-> test-img-url
+      (dtype-img/load )
+      (tensor/ensure-tensor)
+      (tensor/mget 10 10))
+
+  (clj2d-pixels/get-color
+   (clj2d-pixels/to-pixels test-img-clj2d)
+   10 10)
+
+  (def test-cnvs
+    (clj2d/canvas 900 900))
+
+
+  (clj2d/show-window test-cnvs)
+
+
+  (clj2d/with-canvas [c test-cnvs]
+    (clj2d/image c test-img-clj2d)
+    (clj2d/set-color c 0 0 0)
+    (clj2d/rect c 40 40 200 200))
+
+  )
+
+(t/deftest image-processing
+  (t/is
+   (= (.getRGB test-img-clj2d (first test-coords) (second test-coords))
+      (.getRGB test-img-dtype (first test-coords) (second test-coords))))
+
+  (clj2d-color/color (.getRGB test-img-dtype 10 10))
+
+  (let [[x y] test-coords
+        [r g b a]
+        (clj2d-color/color
+         (.getRGB test-img-clj2d x y))
+        [b' g' r'] (test-img-tens x y) ]
+    (t/is
+     (= (mapv int [r g b]) [r' g' b']))))
+
+(comment
+  (clj2d-color/color
+   (.getRGB test-img-clj2d 1000 1000))
+
+  (test-img-tens 1000 1000)
+
+  (dtype-img/image-type test-img-dtype)
+
+  (into [] (clj2d-color/color (.getRGB test-img-dtype (first test-coords) (second test-coords))))
+
+  (unchecked-byte (.getRGB test-img-dtype (first test-coords) (second test-coords)) )
+
   (require '[clojure2d.color :as clj2d-color])
 
   (rgb->hsv [200 100 50])
